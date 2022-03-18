23bc6d95cee89995d926d7df4eb2ef2fe0df3f00
lkrnetic77 <krneticluka@gmail.com>
2022-03-18 13:32:20 +0100

    Added starter files.

---
+++ src/main/java/com/example/SimplifiedReddit/SimplifiedRedditApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimplifiedReddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedRedditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimplifiedReddit/SimplifiedRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimplifiedReddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
